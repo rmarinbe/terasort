8b6ff635fa6d8c027954253d538086f5cf843019
rmarinbe <richardmarin@gmail.com>
2022-04-20 22:38:19 -0400

    Added sample sort code version instead of bucket sort

--- student/terasort.c
+++ student/terasort.c
@@ -84,31 +84,31 @@ void prepare_xdata(terarec_t *local_data, int  local_len, int *bucket_ids, int *
     free(elem_idx);
 }
 
-void calc_counts_and_displs(int *bucket_counts, int *recv_counts, int *recv_displs, int *sorted_counts, long *sorted_displs, int rank, int procs) {
+void populate_sort_info(int *all_counts, int *recv_counts, int *recv_displs, int *sorted_counts, long *sorted_displs, int my_rank, int procs) {
 
     sorted_displs[0] = 0;
     recv_displs[0] = 0;
 
-    for (int i = 0; i < procs; i++)
+    for (int r = 0; r < procs; r++)
     {
-        //calculate recv_counts and recv_displs
-        recv_counts[i] = bucket_counts[i*procs+rank];
-        if(i >0 )
-            recv_displs[i] = recv_displs[i-1] + recv_counts[i-1];
+        //calculate recv_counts and recv_displs, contribution from all 'r' ranks to current rank (me)
+        recv_counts[r] = all_counts[r*procs + my_rank];
+        if(r >0 )
+            recv_displs[r] = recv_displs[r-1] + recv_counts[r-1];
             
-        //calculate sorted_counts and sorted displs
+        //calculate sorted_counts and sorted displs, contributin from all ranks to any rank
         for (int j = 0; j < procs; j++) {
             if(j==0)
-                sorted_counts[i] = 0;
-            sorted_counts[i] += bucket_counts[j*procs+i];
+                sorted_counts[r] = 0;
+            sorted_counts[r] += all_counts[j*procs + r];
         }
-        if(i >0 )
-            sorted_displs[i] = sorted_displs[i-1] + sorted_counts[i-1];
+        if(r >0 )
+            sorted_displs[r] = sorted_displs[r-1] + sorted_counts[r-1];
 
     }
 }
 
-void terasort(terarec_t *local_data, int  local_len, 
+void terasort_bucket(terarec_t *local_data, int  local_len, 
 			  terarec_t **sorted_data, int* sorted_counts, long* sorted_displs){
 	//The code below gathers the data together at a single root node, 
 	//this root node sorts the data and then scatters it back to 
@@ -136,7 +136,7 @@ void terasort(terarec_t *local_data, int  local_len,
     
     MPI_Wait(&request, MPI_STATUS_IGNORE);
     
-    calc_counts_and_displs(bucket_counts, recv_counts, recv_displs, sorted_counts, sorted_displs, rank, P);
+    populate_sort_info(bucket_counts, recv_counts, recv_displs, sorted_counts, sorted_displs, rank, P);
 
     *sorted_data = malloc(sizeof(terarec_t) * sorted_counts[rank]);
     
@@ -144,38 +144,132 @@ void terasort(terarec_t *local_data, int  local_len,
 
 	qsort(*sorted_data, sorted_counts[rank], sizeof(terarec_t), teraCompare);
 
-/*    
-    printf("my rank: %d\n", rank);
-    for (int i = 0; i < P; i++)
-    {
-        printf("rank: %d | ", i);
-        for (int j = 0; j < P; j++)
-        {
-            printf("%d, ", bucket_counts[i*P+j]);
-        }
-        printf("\n");
-        
-    }
-    for (size_t i = 0; i < P; i++)
-    {
-        printf(" %d, %d;", send_displs[i], bucket_counts[P*rank+i]);
-    }
-    
-    printf("\n");
-*/
-
-/*
-    sleep(0.5*rank);
-    for (int i = 0; i < sorted_counts[rank]; i++)
-    {
-        printf("rank: %d , key %s\n", rank, (*sorted_data)[i].key);
-    }
-*/
-
     free(xdata);
     free(bucket_ids);
     free(bucket_counts);
     free(recv_counts);
     free(send_displs);
     free(recv_displs);
+}
+
+void partition_with_splitters(terarec_t *data, int  dat_len, terarec_t *splitters, int spl_len, int *bkt_counts, int *bkt_displs, int procs) {
+
+    int k = 0; // track the splitter num
+    bkt_displs[0] = 0;
+
+    for (int i = 0; i < dat_len; i++) {
+        while(teraCompare(data+i, splitters+k) >= 0 && k < spl_len) {
+            ++k;
+            bkt_displs[k] = i;
+        }
+        ++bkt_counts[k];
+    }
+}
+
+
+void terasort(terarec_t *local_data, int  local_len, 
+			  terarec_t **sorted_data, int* sorted_counts, long* sorted_displs){
+
+    int rank, P;
+
+    MPI_Comm_size (MPI_COMM_WORLD, &P);
+    MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+    
+    int s = P-1; // sample, splitters
+    int d = local_len/P;
+    //corner cases:
+    if(s > local_len) s = local_len-1;
+    if(d==0) d = 1;
+    
+    terarec_t *local_samples = (terarec_t*) malloc(s*sizeof(terarec_t));
+    terarec_t *splitters = (terarec_t*) malloc(s*sizeof(terarec_t));
+    terarec_t *root_samples = NULL;
+    int *all_counts = (int*) calloc( P * P, sizeof(int));
+    int *send_displs = (int*) malloc(P * sizeof(int));
+    int *recv_counts = (int*) calloc(P, sizeof(int));
+    int *recv_displs = (int*) malloc(P * sizeof(int));
+
+    if(rank == 0) root_samples = (terarec_t*) malloc(P*s*sizeof(terarec_t));
+    // 1. Local Sort
+	qsort(local_data, local_len, sizeof(terarec_t), teraCompare);
+    // 2. Select P-1 samples (s)
+    for (int i = 0; i < s; i++)
+        local_samples[i] = local_data[(i+1)*d];
+    // 3. Gather samples at root 
+	MPI_Gather(local_samples, s, mpi_tera_type, root_samples, s, mpi_tera_type, 0, MPI_COMM_WORLD);
+    // 4. Sort samples at root and select splitters
+    if(rank == 0) { 
+        qsort(root_samples, P*s, sizeof(terarec_t), teraCompare);
+        for (int i = 0, x = (P*s)/P; i < s; i++) 
+            splitters[i] = root_samples[(i+1)*x];
+    }
+    // 5. Broadcast splitters
+    MPI_Bcast(splitters, s, mpi_tera_type, 0, MPI_COMM_WORLD);
+    // 6. Partition using splitters
+    partition_with_splitters(local_data, local_len, splitters, s, all_counts+(P*rank), send_displs, P);
+    // 7. Gather all counts
+    MPI_Allgather(MPI_IN_PLACE, 0, MPI_INT, all_counts, P, MPI_INT, MPI_COMM_WORLD);
+    // 8. populate sort info
+    populate_sort_info(all_counts, recv_counts, recv_displs, sorted_counts, sorted_displs, rank, P);
+    // 9. Exchange Values
+    *sorted_data = malloc(sizeof(terarec_t) * sorted_counts[rank]);
+
+    MPI_Alltoallv(local_data, all_counts+(P*rank), send_displs, mpi_tera_type, *sorted_data, recv_counts, recv_displs, mpi_tera_type, MPI_COMM_WORLD);
+    // 10. final local sort
+	qsort(*sorted_data, sorted_counts[rank], sizeof(terarec_t), teraCompare);
+    
+
+/*
+    sleep(0.5*rank);
+    printf("\nLocal Keys \n");
+    for (int i = 0; i < local_len; i++)
+    {
+        printf("rank %d key %s \n", rank, local_data[i].key);
+    }
+    
+    if(rank==0) {
+    printf("\nRoot Samples\n");
+    for (size_t i = 0; i < P*s; i++)
+    {
+        printf("root %s\n", root_samples[i].key); 
+    }
+    }
+    
+    printf("\nAll Counts\n");
+    for (int i = 0; i < P*P; i++)
+    {
+       printf("rank %d counts %d\n", rank, all_counts[i]);
+    }
+    
+    printf("\nSplitters\n");
+    for (size_t i = 0; i < s; i++)
+    {
+        printf("rank %d split %s\n", rank, splitters[i].key); 
+    }
+    printf("\nTo Send\n");
+    for (size_t i = 0; i < P; i++)
+    {
+        printf("rank %d displ %d counts %d\n", rank, send_displs[i], all_counts[P*rank+i]); 
+    }
+    
+    printf("\nSorted keys disp %ld counts %d \n", sorted_displs[rank], sorted_counts[rank]);
+    for (int i = 0; i < sorted_counts[rank]; i++)
+    {
+        printf("rank %d sorted key %s \n", rank, (*sorted_data)[i].key);
+    }
+    printf("\n--------------\n");
+*/
+    
+    free(local_samples);
+    free(splitters);
+    if(rank ==0) {
+        free(root_samples);
+    }
+    free(all_counts);
+    free(recv_counts);
+    free(send_displs);
+    free(recv_displs);
+
+    
+    
 }
